8f6e5dbe87ddf74a89653dabdf3697f7134be518
Krista Korento <krista.korento@gmail.com>
2025-03-21 11:25:07 +0200

    Routerit kaikille projektin nettisivuille luotu ja yhdistetty.

--- Animal Rescue Adoption Website/src/App.jsx
+++ Animal Rescue Adoption Website/src/App.jsx
@@ -1,8 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import FrontPage from './Nettisivujen Koodit/FrontPage'
+import TrialAdoption from './Nettisivujen Koodit/TrialAdoption'
+import PrivacyPolicy from './Nettisivujen Koodit/PrivacyPolicy'
+import PreApproval from './Nettisivujen Koodit/PreApproval'
+import Policies from './Nettisivujen Koodit/Policies'
+import InfoGuides from './Nettisivujen Koodit/InfoGuides'
+import FeesAndProcess from './Nettisivujen Koodit/FeesAndProcess'
+import FAQ from './Nettisivujen Koodit/FAQ'
+import DogProfiles from './Nettisivujen Koodit/DogProfiles'
+import ContactDetails from './Nettisivujen Koodit/ContactDetails'
+import Blogs from './Nettisivujen Koodit/Blogs'
+import Adoption from './Nettisivujen Koodit/Adoption'
 
 function App() {
   return(
@@ -16,15 +25,51 @@ function App() {
 const myRouter = createBrowserRouter([
   {
     path: '/',
-    element: <Home/>
+    element: <FrontPage/>
   },
   {
-    path: '/profile',
-    element: <Profile/>
+    path: '/trial-adoption',
+    element: <TrialAdoption/>
   },
   {
-    path: '/news',
-    element: <News/>
+    path: '/privacy-policy',
+    element: <PrivacyPolicy/>
+  },
+  {
+    path: '/pre-approval',
+    element: <PreApproval/>
+  },
+  {
+    path: '/policies',
+    element: <Policies/>
+  },
+  {
+    path: '/info-guides',
+    element: <InfoGuides/>
+  },
+  {
+    path: '/fees-and-process',
+    element: <FeesAndProcess/>
+  },
+  {
+    path:'/faq',
+    element: <FAQ/>
+  },
+  {
+    path: '/dog-profiles',
+    element:<DogProfiles/>
+  },
+  {
+    path: '/contact-details',
+    element: <ContactDetails/>
+  },
+  {
+    path: '/blogs',
+    element: <Blogs/>
+  },
+  {
+    path: '/adoption',
+    element:<Adoption/>
   }
 ])
 
